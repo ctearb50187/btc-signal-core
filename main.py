c8ad065b6648ef600a29a9a8d10b743fba4f6263
ctearb50187 <poeawq99871@gmail.com>
2025-05-03 03:39:14 +0800

    Add files via upload

--- main.py
+++ main.py
@@ -1,15 +1,5 @@
-
-from fastapi import FastAPI, Request
-from fastapi.responses import FileResponse
-import os
-
+from fastapi import FastAPI
 app = FastAPI()
-
-@app.get("/")
-def homepage():
-    return FileResponse("index.html")
-
-@app.post("/ai_predict")
-async def ai_predict(request: Request):
-    data = await request.json()
-    return {"prediction": "buy"}  # 模擬回傳值，後續可替換為 AI 引擎
+@app.get('/')
+def root():
+ return {'status':'ok'}
